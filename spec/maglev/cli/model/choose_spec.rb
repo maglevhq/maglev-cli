33a622faf512baab42a29d7fe4274a9ec2d4dd49
Rodrigo Alvarez <papipo@gmail.com>
2020-09-30 10:18:34 +0200

    Add model chooser.

---
+++ spec/maglev/cli/model/choose_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tty/prompt'
+require 'maglev/cli/model'
+require 'maglev/cli/model/choose'
+
+RSpec.describe Maglev::CLI::Model::Choose do
+  let(:chosen_model) { Maglev::CLI::Model.new(name: 'User', path: 'app/models/user.rb') }
+  let(:prompt) { instance_double(TTY::Prompt) }
+  let(:choices) { [{ name: chosen_model.name, value: chosen_model }] }
+
+  before do
+    allow(prompt).to receive(:select).with('Which model will have maglev sites?', choices).and_return chosen_model
+  end
+
+  subject { described_class.call(models: [chosen_model], prompt: prompt) }
+
+  it { is_expected.to eq(chosen_model) }
+end
