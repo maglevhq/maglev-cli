abbaa258627920f6936b107a82f3de9cac37a236
Rodrigo Alvarez <papipo@gmail.com>
2020-10-13 13:52:37 +0200

    Add `current_site` entry to example config.

--- lib/maglev/cli/templates/install/config/maglev.rb
+++ lib/maglev/cli/templates/install/config/maglev.rb
@@ -4,4 +4,5 @@ Maglev.configure do |config|
   # config.logo = 'logo.png'
   # config.favicon = 'favicon.ico'
   # config.primary_color = '#2F4F4F'
+  # config.current_site = -> { current_user.maglev_site }
 end
