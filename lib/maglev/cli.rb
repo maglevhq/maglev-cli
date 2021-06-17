ccc8612eaf02bf08ad94ff32b4c303b5e857bbdc
Rodrigo Alvarez <papipo@gmail.com>
2021-06-17 12:44:09 +0200

    Don't display internal commands in cli.

--- lib/maglev/cli.rb
+++ lib/maglev/cli.rb
@@ -2,13 +2,13 @@
 
 require 'thor'
 require 'bundler/cli'
-require_relative 'cli/setup'
 
 module Maglev
   # The main CLI entrypoint
   class CLI < Thor
     desc 'setup', 'Run this in a Rails app folder to set up Maglev'
     def setup
+      require_relative 'cli/setup'
       Maglev::CLI::Setup.start
     end
   end
